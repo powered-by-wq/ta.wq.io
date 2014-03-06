e5162bebf22bd2521aa589c85446ca50a46b2102
S. Andrew Sheppard <andrew@wq.io>
2014-03-05 18:04:45 -0600

    v0.3.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     install_requires=['wq.app>=0.5.0', 'pyyaml', 'pystache'],
     scripts=['tawq/bin/tawq'],
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
